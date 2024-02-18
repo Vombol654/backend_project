1d3710eb616fe2eaea4c2d1c23c626209ca9d6d3
Vombol@654 <ananda.learn654@gmail.com>
2024-02-18 11:33:43 +0530

    after adding utility-files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 require("dotenv").config({ path: `${__dirname}/.env` });
 const connect_db = require("./db/index");
-connect_db.connectDB();
+const app=require("./app")
+connect_db.connectDB().then(()=>{
+    app.listen(process.env.PORT||8080,()=>{
+        console.log(`server is running at ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("MongoDB connection failed",err)
+});
